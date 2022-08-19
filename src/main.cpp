7e09d497f8241a47b850dbe0622b6a81ff6dfb8d
JabMich <jablonaack@gmail.com>
2022-08-19 13:02:43 +0200

    added support for BME280 sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
 #include <Arduino.h>
 
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+//#include <Adafruit_BMP280.h>
+
+#define SEA_LEVEL_PRESSURE_HPA (1013.25) // TO ADJUST
+
+Adafruit_BME280 bme;
+
+int i2c_bme_address = 0x76;
+
 #define BLYNK_PRINT Serial
 
 #include <Adafruit_NeoPixel.h>
@@ -36,19 +48,23 @@ void rainbow(uint8_t wait) {
     delay(wait);
   }
 }
+void setSlidersLevel(){
+  Blynk.virtualWrite(V5,red);
+  Blynk.virtualWrite(V6,green);
+  Blynk.virtualWrite(V7,blue);
+}
 
 void changeColor(){
   for(int i =0; i < strip.numPixels(); i++)
     strip.setPixelColor(i,red,green,blue);
   strip.show();
+  setSlidersLevel();
 }
-
 
 BLYNK_WRITE(V4) {  // set color for RGB_zebra
   red = param[0].asInt();
   green = param[1].asInt();
   blue = param[2].asInt();
-
   changeColor();
 }
 
@@ -67,34 +83,57 @@ BLYNK_WRITE(V1) { // ON rainbow
   delay(10);
 }
 
-BLYNK_WRITE(V2){
-  int shift = param.asInt();
-  strip.setBrightness(shift);
-  strip.show();          
-}
-
-BLYNK_WRITE(V5){
+BLYNK_WRITE(V5){ // red color slider
   red = param.asInt();
   changeColor();
 }
 
-BLYNK_WRITE(V6){
+BLYNK_WRITE(V6){ // green color slider
   green = param.asInt();
   changeColor();
 }
 
-BLYNK_WRITE(V7){
+BLYNK_WRITE(V7){ // blue color slider
   blue = param.asInt(); 
   changeColor();
+}
+
+BLYNK_READ(V10){ // temperature 
+  float temperature = bme.readTemperature();
+  Blynk.virtualWrite(V10,temperature);
+}
+
+BLYNK_READ(V11){ // pressure
+  float pressure = bme.readPressure() / 100.0F;
+  Blynk.virtualWrite(V11,pressure);
+}
+
+BLYNK_READ(V12){ // humidity
+  float humidity = bme.readHumidity();
+  Blynk.virtualWrite(V12,humidity);
 }
 
 void setup(){
   Serial.begin(9600);
 
+  unsigned status = bme.begin(i2c_bme_address);
+
   Blynk.begin(auth, ssid, pass, IPAddress(ip_address[0],ip_address[1],ip_address[2],ip_address[3]), 8080);
+
+  Serial.println(status);
 
   strip.begin();
   strip.show();
+
+   if (!status) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+        Serial.print("        ID of 0x60 represents a BME 280.\n");
+        Serial.print("        ID of 0x61 represents a BME 680.\n");
+        //while (1) delay(10);
+    }
 }
 
 void loop(){
